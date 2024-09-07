0d92e7fabfc7dd515cd12b24428e2d1c0898ae1d
IPOleksenko <1ipooip1@gmail.com>
2024-09-07 09:24:54 +0300

    migration to react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./background.css";
+import "./title.css";
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <div id="title">
+          Weather <small>Site</small>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
